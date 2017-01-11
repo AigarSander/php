99dae95281b7ee2be0b4138580b72e9c40147e62
AigarSander <aigar.sander@khk.ee>
2017-01-11 14:55:41 +0200

    php class test

---
+++ oop/test.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: B50-50
+ * Date: 11.01.2017
+ * Time: 14:54
+ */
+
+require_once ('text.php');
+
+$sentence = new text();
+echo '<pre>';
+print_r($sentence);
+echo '</pre>';
+?>
