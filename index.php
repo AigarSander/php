9f7cd2502f680ee6e464fce4735fdd242c225447
aixleaix <underteh@outlook.com>
2017-01-12 13:06:29 +0200

    template push 1.1

---
+++ index.php
@@ -0,0 +1,21 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: B50-50
+ * Date: 12.01.2017
+ * Time: 12:58
+ */
+define('CLASSES_DIR', 'classes/');
+define('TMPL_DIR', 'tmpl/');
+require_once (CLASSES_DIR.'template.php');
+
+$tmpl = new template();
+$tmpl->file = 'main.html';
+echo '<pre>';
+print_r($tmpl);
+echo '</pre>';
+$tmpl->loadFile();
+echo '<pre>';
+print_r($tmpl);
+echo '</pre>';
+?>
