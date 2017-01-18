9a0049471f4920dd3f372417a654a872c7cafc76
aixleaix <underteh@outlook.com>
2017-01-18 09:14:03 +0200

    PHP push 1.6

--- classes/linkObject.php
+++ classes/linkObject.php
@@ -9,5 +9,13 @@
 require_once ('http.php');
 
 class linkObject extends http{
+    var $baseUrl = false;
+    var $protocol = 'http://';
+    var $delim = '&amp';
+    var $eq = '=';
 
+    function __construct() {
+        parent::__construct();
+        $this->baseUrl = $this->protocol.HTTP_HOST.SCRIPT_NAME;
+    }
 }
