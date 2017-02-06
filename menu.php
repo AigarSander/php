4c3280a4f4e9096f9a5fb70b61144ea8681f27df
aixleaix <underteh@outlook.com>
2017-02-06 08:56:25 +0200

    Edited menu.php

--- menu.php
+++ menu.php
@@ -6,18 +6,31 @@
  * Time: 10:34
  */
 $menu = new template('menu.menu');
+$menu->set('items', false);
 $item = new template('menu.item');
 
-$sql = 'select content_id, title from content where parent_id="0" AND show_in_menu="1" ORDER BY sort ASC';
+$sql = 'select content_id, title from content where parent_id="0" AND show_in_menu="1"';
+
+if(ROLE_ID != ROLE_ADMIN) {
+    $sql .= 'AND is_hidden = 0';
+}
+$sql .= ' ORDER BY sort ASC';
 $res = $db->getArray($sql);
 
 if($res != false) {
     foreach ($res as $page) {
-        $item->set('name', $page['title']);
         $link = $http->getLink(array('page_id'=>$page['content_id']));
         $item->set('link', $link);
-
+        $item->set('name', $page['title']);
         $menu->add('items', $item->parse());
     }
 }
+
+if(USER_ID != ROLE_NONE) {
+    $link = $http->getLink(array('act' => 'logout'));
+    $item->set('link', $link);
+    $item->set('name', 'LOG OUT');
+    $menu->add('items', $item->parse());
+}
+$tmpl->set('menu', $menu->parse());
 ?>
