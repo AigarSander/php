f7b0caa0824f7a8c09b7f51bf81091e474b576ab
aixleaix <underteh@outlook.com>
2017-02-06 08:56:02 +0200

    Index edited

--- index.php
+++ index.php
@@ -12,12 +12,12 @@ $tmpl = new template('main');
 $tmpl->set('title', 'TITLE');
 
 require_once 'menu.php';
-$tmpl->set('menu', $menu->parse());
 
 $tmpl->set('nav_bar', 'NAVIGATION');
 $tmpl->set('lang_bar', 'LANGUAGE');
-$tmpl->set('content', $http->get('content'));
-echo $tmpl->parse();
 
 require_once 'act.php';
+$tmpl->set('content', $http->get('content'));
+
+echo $tmpl->parse();
 ?>
