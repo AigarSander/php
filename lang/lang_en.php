6feb7bf7f3f85eb291e715e246e553382503cf68
aixleaix <underteh@outlook.com>
2017-02-06 09:49:05 +0200

    Added translations

--- lang/lang_en.php
+++ lang/lang_en.php
@@ -5,3 +5,14 @@
  * Date: 06.02.2017
  * Time: 9:36
  */
+
+$_trans = array(
+    'Esimene' => 'First',
+    'Teine' => 'Second',
+    'Minu lehe pealkiri' => 'My page heading',
+    'Logi välja' => 'Logout',
+    'Kasutajanimi' => 'Username',
+    'Parool' => 'Password',
+    'Logi sisse' => 'Login',
+    'Viga sisselogimisel' => 'Login error'
+);
