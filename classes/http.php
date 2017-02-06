c22dd63844844290cedb778a963e9595d8309925
aixleaix <underteh@outlook.com>
2017-02-06 08:23:27 +0200

    Code fix

--- classes/http.php
+++ classes/http.php
@@ -7,48 +7,59 @@
  * Time: 12:48
  */
 
-function fixHtml($val) {
+function fixHtml($val){
     return htmlentities($val);
-}
-
-class http {
+}// fixHtml
+class http
+{
     var $server = array();
     var $vars = array();
     var $cookie = array();
 
-    function __construct() {
+    function __construct()
+    {
         $this->init();
         $this->initConst();
     }
 
-    function init() {
+    function init()
+    {
         $this->server = $_SERVER;
         $this->cookie = $_COOKIE;
         $this->vars = array_merge($_GET, $_POST, $_FILES);
     }
 
-    function initConst() {
+    function initConst()
+    {
         $vars = array('REMOTE_ADDR', 'PHP_SELF', 'SCRIPT_NAME', 'HTTP_HOST');
-        foreach($vars as $var) {
-            if(!defined($var) and isset($this->server[$var])) {
+        foreach ($vars as $var) {
+            if (!defined($var) and isset($this->server[$var])) {
                 define($var, $this->server[$var]);
             }
         }
     }
 
-    function set($name, $value) {
-        $this->vars[$name] = $value;
+    function set($name, $val)
+    {
+        $this->vars[$name] = $val;
     }
 
-    function get($name, $fix = true) {
-        if(isset($this->vars[$name])) {
-            if($fix) {
+    function get($name, $fix = true)
+    {
+        if (isset($this->vars[$name])) {
+            if ($fix) {
                 return fixHtml($this->vars[$name]);
             }
-
             return $this->vars[$name];
         }
 
         return false;
+    }
+
+    function del($name)
+    {
+        if (isset($this->vars[$name])) {
+            unset($this->vars[$name]);
+        }
     }
 }
