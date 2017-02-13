429138a16ebf181406691591b9ea12cdaf5d46c6
aixleaix <underteh@outlook.com>
2017-02-13 12:53:21 +0200

    Implement login/logout system for admins

--- acts/login.php
+++ acts/login.php
@@ -5,6 +5,15 @@
  * Date: 13.02.2017
  * Time: 12:14
  */
-$sess->createSession();
-$http->redirect();
+$form = new Template('login');
+$form->set('error', $sess->get('login_error'));
+$sess->del('login_error');
+$form->set('submit', ('Logi sisse'));
+$form->set('username_str', ('Kasutajanimi'));
+$form->set('password_str', ('Parool'));
+$form->set('username', $http->get('username', true));
+$link = $http->getLink(array('act' => 'login_do'));
+$form->set('action', $link);
+//echo $form->parse();
+$tmpl->set('content', $form->parse());
 ?>
